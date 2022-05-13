871108421d966f44495b84a1f0147e716408fbfb
SURYA NARAYANAN S <101622233+suryastark251@users.noreply.github.com>
2022-05-13 22:25:25 +0530

    Update stm32f4xx_it.c

--- 3_Implementation/src/stm32f4xx_it.c
+++ 3_Implementation/src/stm32f4xx_it.c
@@ -1,23 +1,3 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    stm32f4xx_it.c
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
